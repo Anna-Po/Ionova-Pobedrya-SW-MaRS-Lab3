c7fa26cf946216ba0d317c13becfdf0a50e0ba09
Anna <pobedria4@gmail.com>
2022-01-22 17:47:31 +0300

    first commit

---
+++ Temp/Includes/Globalvar.h
@@ -0,0 +1,38 @@
+/* Automation Studio generated header file */
+/* Do not edit ! */
+
+#ifndef _BUR_1504106080_2_
+#define _BUR_1504106080_2_
+
+#include <bur/plctypes.h>
+
+/* Constants */
+#ifdef _REPLACE_CONST
+#else
+#endif
+
+
+/* Variables */
+_GLOBAL struct DoorSM door;
+_GLOBAL struct LedControl led3;
+_GLOBAL struct LedControl led2;
+_GLOBAL struct LedControl led1;
+_GLOBAL struct LedControl led0;
+
+
+
+
+
+__asm__(".section \".plc\"");
+
+/* Used IEC files */
+__asm__(".ascii \"iecfile \\\"Logical/Global.var\\\" scope \\\"global\\\"\\n\"");
+__asm__(".ascii \"iecfile \\\"Logical/Libraries/DriveLib/DriveLib.fun\\\" scope \\\"global\\\"\\n\"");
+
+/* Exported library functions and function blocks */
+
+__asm__(".previous");
+
+
+#endif /* _BUR_1504106080_2_ */
+
